a413d3112619cbb81a3f657cfac4109205a0e328
JinshengPeng <jinsheng.peng@pscconsulting.com>
2020-08-25 23:46:28 +0100

    GUI init

--- pf_control/__init__.py
+++ pf_control/__init__.py
@@ -1,19 +1,24 @@
 """
 #######################################################################################################################
 ###											Initialisation															###
-###		Script deals with initialising the PF_Controlscripts														###
+###		Script deals with initialising the PF_Control scripts														###
 ###																													###
-###		Code developed by David Mills (david.mills@pscconsulting.com, +44 7899 984158) as part of PSC UK Ltd. 		###
+###		Code developed by Jinsheng Peng (jinsheng.peng@pscconsulting.com, +44 7899 984158) as part of PSC UK Ltd. 		###
 ###																													###
 #######################################################################################################################
 """
 import importlib
-import sys
+import logging
 
-import pf_control
 import pf_control.constants as constants
 import pf_control.gui as gui
+import pf_control.pf as pf
 
 # Reload all modules
 constants = importlib.reload(constants)
 gui = importlib.reload(gui)
+
+if constants.logger is None:
+	logging.basicConfig()
+	constants.logger = logging.getLogger()
+	constants.logger.setLevel(level=logging.DEBUG)
